fb7ca75c7473f599d8f5cea0e0644421d7be5f4e
Jatoth Adithya Naik <jatothadithyanaik@gmail.com>
2026-04-03 23:26:02 +0530

    feat(auth): setup authentication, create controller and route (#6)
    * feat(auth): setup authentication, create controller and route
    
    * feat(auth): setup authentication, create controller and route
    
    * fix:auto-labeller and auto-assign trigger
    
    * fix : errors
    
    * Update server/index.js
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Update server/controllers/auth.controller.js
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * coderabbit suggestions
    
    * coderabbit suggestions
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import dbConnect from "./config/db.js";
 dotenv.config();
+import dbConnect from "./config/db.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
+
+app.use("/api/auth",authRouter)
 
 const startServer = async () => {
    try {
