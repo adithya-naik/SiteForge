1191feb71bd85cef468d0f53d81dd1523f6ee057
adithya-naik <jatothadithyanaik@gmail.com>
2026-04-04 16:39:31 +0530

    middleware setup for auth

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -24,7 +24,7 @@ export const googleAuth = async (req, res) => {
       // secure:true,
       sameSite: 'strict',
       // sameSite:"none",
-      maxAge: 7 * 24 * 60 * 60 * 1000
+      maxAge: 3 * 24 * 60 * 60 * 1000
     });
 
     return res.status(200).json({
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dbConnect from './config/db.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import userRouter from './routes/user.route.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.use(cors({
 }));
 
 app.use('/api/auth',authRouter);
+app.use('/api/user',userRouterRouter);
 
 const startServer = async () => {
    try {
